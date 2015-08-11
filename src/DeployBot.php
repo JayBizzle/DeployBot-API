8125bf2f373d64478f9bb1ebe6c3fa327167c77e
Mark Beech <mbeech@mark-beech.co.uk>
2015-08-11 20:15:43 +0100

    Finalising full API functionality

--- src/DeployBot.php
+++ src/DeployBot.php
@@ -3,12 +3,14 @@
 namespace Jaybizzle;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 
 class DeployBot
 {
     private $api_key;
     private $api_endpoint = 'https://<account>.deploybot.com/api/v1/';
     private $client;
+    private $query = [];
 
     public function __construct($api_key, $account)
     {
@@ -19,34 +21,121 @@ class DeployBot
             'base_uri' => $this->api_endpoint,
             'headers' => [
                 'X-Api-Token' => $this->api_key,
-                'Accept' => 'application/json'
+                'Accept' => 'application/json',
             ],
-            'debug' => false
+            'debug' => false,
         ]);
     }
 
-    public function __call($method, $args)
+    /**
+     * Dynamically add query parameters or call API endpoints.
+     * 
+     * @param string $method
+     * @param array  $args
+     *
+     * @return Object
+     */
+    public function __call($name, $args)
     {
-        return $this->makeRequest($method, $args);
+        if (substr($name, 0, 3) == 'get') {
+            $name = strtolower(substr($name, 3));
+            return $this->buildRequest($name, $args);
+        } else {
+            return $this->addQuery($name, $args);
+        }
     }
 
-    private function makeRequest($method, $args = array())
+    /**
+     * Trigger a deployment.
+     * 
+     * @return JSON
+     */
+    public function triggerDeployment()
+    {
+        $this->buildRequest('deployments', [], 'post');
+    }
+
+    /**
+     * Add query parameters.
+     * 
+     * @param string $method
+     * @param array  $args
+     */
+    private function addQuery($name, $args)
+    {
+        $name = $this->snakeCase($name);
+
+        $this->query[$name] = $args[0];
+
+        return $this;
+    }
+
+    /**
+     * Prepare the request.
+     * 
+     * @param string $resource
+     * @param array  $args
+     * @param string $method
+     *
+     * @return JSON
+     */
+    private function buildRequest($resource, $args = [], $method = 'get')
     {
         $query = [];
-        if(count($args[0]) == 1 && is_int($args[0])) {
-            $method = $method.'/'.$args[0];
+
+        if (count($args[0]) == 1 && is_int($args[0])) {
+            $resource = $resource.'/'.$args[0];
         }
 
-        if(isset($args[1]['query'])) {
-            $query = $args[1]['query'];
+        if (!empty($this->query)) {
+            $query = $this->query;
         }
 
-         if(isset($args[0]['query'])) {
-            $query = $args[0]['query'];
+        return $this->sendRequest($resource, $query, $method);
+    }
+
+    /**
+     * Send the request.
+     * 
+     * @param string $resource
+     * @param array  $args
+     * @param string $method
+     *
+     * @return JSON
+     */
+    private function sendRequest($resource, $query = [], $method = 'get')
+    {
+        $option_name = ($method == 'get') ? 'query' : 'json';
+
+        try {
+            $response = $this->client->$method($resource, [$option_name => $query]);
+        } catch (ClientException $e) {
+            return $e->getResponse()->getBody()->getContents();
         }
 
-        $response = $this->client->get($method, ['query' => $query]);
+        // Reset query parameters
+        $this->query = [];
 
         return json_decode($response->getBody()->getContents());
+    }
+
+    /**
+     * Convert camelCase methods to snake_case params.
+     * 
+     * @param string $value
+     * @param string $delimiter
+     *
+     * @return string
+     */
+    private function snakeCase($value, $delimiter = '_')
+    {
+        $key = $value.$delimiter;
+
+        if (!ctype_lower($value)) {
+            $value = strtolower(preg_replace('/(.)(?=[A-Z])/', '$1'.$delimiter, $value));
+            $value = preg_replace('/\s+/', '', $value);
+        }
+
+        return $value;
     }
 }
